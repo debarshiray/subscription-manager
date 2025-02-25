debarshiray/subscription-manager

commit 8dddb545597e31a45304bec0d33e89e52a8fa705

Fix building libdnf-plugin RPM; ENT-3192
* When Fedora 33 or higher is used, then RPM containing only
  libdnf plugin is build (libdnf-plugin-subscription-manager)
  and DNF plugins (product-id and subscription-manager) are
  part of subscription-manager RPM
* When RHEL8 is used, then dnf and libdnf modules are included
  in dnf-plugin-subscription-manager RPM.
* This approach makes future cherry-picking easier

subscription-manager.spec
@@ -100,6 +100,7 @@
 %endif
 
 %global use_dnf (%{with python3} && (0%{?fedora} || (0%{?rhel}))) || (0%{?rhel} >= 7)
+%global create_libdnf_rpm (0%{?fedora} > 32 || 0%{?rhel} > 8)
 %global use_yum (0%{?rhel} && 0%{?rhel} <= 7)
 %global use_cockpit 0%{?fedora} || 0%{?rhel} >= 7
 
@@ -322,8 +323,14 @@ Requires: %{?suse_version:dbus-1-python} %{!?suse_version:dbus-python}
 Requires: %{?suse_version:yum} %{!?suse_version:yum >= 3.2.29-73}
 %endif
 
-%if (%{use_dnf} && (0%{?fedora} || 0%{?rhel} >= 8))
+%if %{use_dnf}
+%if %{create_libdnf_rpm}
+Requires: dnf >= 1.0.0
+Requires: python3-dnf-plugins-core
+Requires: python3-librepo
+%else
 Requires: dnf-plugin-subscription-manager = %{version}
+%endif
 %endif
 
 # Support GTK2 and GTK3 on both SUSE and RHEL...
@@ -397,6 +404,17 @@ BuildRequires: systemd
 
 %if !%{use_container_plugin}
 Obsoletes: subscription-manager-plugin-container
+%endif
+
+%if %{use_dnf}
+%if %{create_libdnf_rpm}
+# The libdnf plugin is in separate RPM, but shubscription-manager should be dependent
+# on this RPM, because somebody can install microdnf on host and installing of product
+# certs would not work as expected without libdnf plugin
+Requires: libdnf-plugin-subscription-manager = %{version}
+# The dnf plugin is now part of subscription-manager
+Obsoletes: dnf-plugin-subscription-manager
+%endif
 %endif
 
 %description
@@ -513,10 +531,11 @@ This package contains scripts that aid in moving to certificate based
 subscriptions
 %endif
 
-
-%if %use_dnf
-%package -n dnf-plugin-subscription-manager
-Summary: Subscription Manager plugins for DNF
+%if %{use_dnf}
+# RPM containing DNF plugin
+%if %{create_libdnf_rpm}
+%package -n libdnf-plugin-subscription-manager
+Summary: Subscription Manager plugin for libdnf
 %if 0%{?suse_version}
 Group: Productivity/Networking/System
 %else
@@ -547,10 +566,33 @@ Requires: python2-librepo
 %endif
 Requires: dnf >= 1.0.0
 
+%description -n libdnf-plugin-subscription-manager
+This package provides a plugin to interact with repositories from the Red Hat
+entitlement platform; contains only one product-id binary plugin used by
+e.g. microdnf.
+%else
+# RPM containing DNF plugin
+%package -n dnf-plugin-subscription-manager
+Summary: Subscription Manager plugins for DNF
+%if 0%{?suse_version}
+Group: Productivity/Networking/System
+%else
+Group: System Environment/Base
+%endif
+BuildRequires: cmake
+BuildRequires: gcc
+BuildRequires: json-c-devel
+BuildRequires: libdnf-devel >= 0.22.5
+Requires: json-c
+Requires: libdnf >= 0.22.5
+Requires: python3-dnf-plugins-core
+Requires: python3-librepo
+Requires: dnf >= 1.0.0
 %description -n dnf-plugin-subscription-manager
 This package provides plugins to interact with repositories and subscriptions
 from the Red Hat entitlement platform; contains subscription-manager and
 product-id plugins.
+%endif
 %endif
 
 
@@ -750,7 +792,7 @@ make -f Makefile VERSION=%{version}-%{release} CFLAGS="%{optflags}" \
 python2 ./setup.py build --quiet --gtk-version=%{?gtk3:3}%{?!gtk3:2} --rpm-version=%{version}-%{release}
 %endif
 
-%if (%{use_dnf} && (0%{?fedora} >= 29 || 0%{?rhel} >= 8))
+%if %{use_dnf}
 pushd src/dnf-plugins/product-id
 %cmake -DCMAKE_BUILD_TYPE="Release"
 %if (0%{?rhel} && 0%{?rhel} <= 8)
@@ -779,7 +821,7 @@ make -f Makefile install VERSION=%{version}-%{release} \
     %{?include_syspurpose:INCLUDE_SYSPURPOSE="1"} \
     %{?exclude_packages}
 
-%if (%{use_dnf} && (0%{?fedora} >= 29 || 0%{?rhel} >= 8))
+%if %{use_dnf}
 pushd src/dnf-plugins/product-id
 mkdir -p %{buildroot}%{_libdir}/libdnf/plugins
 %if (0%{?rhel} && 0%{?rhel} <= 8)
@@ -1032,6 +1074,11 @@ find %{buildroot} -name \*.py* -exec touch -r %{SOURCE0} '{}' \;
     %{_prefix}/lib/yum-plugins/search-disabled-repos.py*
 %endif
 
+# When libdnf rpm is created, then dnf plugin is part of subscription-manager rpm
+%if %{create_libdnf_rpm}
+%{python_sitelib}/dnf-plugins/*
+%endif
+
 # zypper plugins
 %if 0%{?suse_version}
 %{_prefix}/lib/zypp/plugins/services/rhsm
@@ -1270,11 +1317,18 @@ find %{buildroot} -name \*.py* -exec touch -r %{SOURCE0} '{}' \;
 %endif
 
 
-%if %use_dnf
+%if %{use_dnf}
+# libdnf RPM
+%if %{create_libdnf_rpm}
+%files -n libdnf-plugin-subscription-manager
+%defattr(-,root,root,-)
+%{python_sitelib}/dnf-plugins/*
+%{_libdir}/libdnf/plugins/product-id.so
+%else
+# DNF RPM
 %files -n dnf-plugin-subscription-manager
 %defattr(-,root,root,-)
 %{python_sitelib}/dnf-plugins/*
-%if (0%{?fedora} >= 29 || 0%{?rhel} >= 8)
 %{_libdir}/libdnf/plugins/product-id.so
 %endif
 %endif

commit e67c8454488b52f9c30d549643c07660f4ee75fe

spec: Remove an unnecessary explicit dependency on json-c
The dnf-plugin-subscription-manager subpackage is only built on RHEL 8
and older RHELs, and the latest RHEL 8 build [1] already contains an
automatically generated dependency on libjson-c.so.4, which is the only
thing provided by the json-c package.  According to the Fedora packaging
guidelines [2], packages MUST NOT contain unnecessary explicit library
dependencies.

Fallout from 8dddb545597e31a45304bec0d33e89e52a8fa705

[1] dnf-plugin-subscription-manager-1.28.42-1.el8.x86_64.rpm

[2] https://docs.fedoraproject.org/en-US/packaging-guidelines/

subscription-manager.spec
@@ -268,7 +268,6 @@ BuildRequires: cmake
 BuildRequires: gcc
 BuildRequires: json-c-devel
 BuildRequires: libdnf-devel >= 0.22.5
-Requires: json-c%{?_isa}
 Requires: libdnf%{?_isa} >= 0.22.5
 %endif
 